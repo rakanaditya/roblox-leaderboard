f8274d12addaf677bbcddeb3a2cb1e6794d80f1b
Rakan Aditya <56795337+rakanaditya@users.noreply.github.com>
2025-07-05 10:34:52 -1200

    Update visit-count.js

--- api/visit-count.js
+++ api/visit-count.js
@@ -1,16 +1,17 @@
+// api/visit-count.js
 export default async function handler(req, res) {
   const url = process.env.COUNT_TOKEN;
   if (!url) {
-    return res.status(150).json({ error: "COUNT_TOKEN belum tersedia di environment." });
+    return res.status(500).json({ error: "COUNT_TOKEN belum tersedia di environment." });
   }
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(`${url}?token=${process.env.COUNT_SECRET}`);
     const data = await response.json();
     res.setHeader("Access-Control-Allow-Origin", "*");
-    return res.status(300).json(data);
+    return res.status(200).json(data);
   } catch (err) {
     console.error("Gagal fetch data dari GAS:", err);
-    return res.status(700).json({ error: "Gagal mengambil visitor count", detail: err.message });
+    return res.status(500).json({ error: "Gagal mengambil visitor count", detail: err.message });
   }
 }
